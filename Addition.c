a73787303717667619326242959a81c08ba325b9
sadhana <149290697+sadhanathanga@users.noreply.github.com>
2024-07-27 14:24:12 +0530

    Create Addition.c

---
+++ Addition.c
@@ -0,0 +1,11 @@
+Code for addition of two numbers
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int a,b,sum;
+printf("Enter two numbers:);
+scanf("%d %d",&a,&b);
+sum=a+b;
+printf("Sum=%d",sum);
+}
